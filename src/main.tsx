fb66e7afb5f6526631039008c27c492c27fe90c2
SonghaiFan <songhai.fan2022@gmail.com>
2023-09-11 20:16:53 +1000

    add basename to router to fix github page issues

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/user_study_narrative_sketch">
       <App />
     </Router>
   </React.StrictMode>
