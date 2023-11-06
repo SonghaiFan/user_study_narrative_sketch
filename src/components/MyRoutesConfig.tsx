6777e802de1d6e44dcd093202b6ecdad43325dd7
SonghaiFan <songhai.fan2022@gmail.com>
2023-11-07 06:25:52 +1100

    Update MyRoutesConfig.tsx
    fix share state issues

--- src/components/MyRoutesConfig.tsx
+++ src/components/MyRoutesConfig.tsx
@@ -10,6 +10,10 @@ import stories from "./data/stories/combined_stories.json";
 export const train_stories_shuffle = _.shuffle(train_stories);
 export const stories_shuffle = _.shuffle(stories);
 
+// Router v5+ doesn't remount the component if the same component is rendered on route change, the state is preserved as long as the component stays mounted. This can make it seem like state is shared if you're expecting a fresh state on every route change.
+
+// To ensure that a fresh instance of SelectionTask with its own state is created every time you navigate to it, you should use the key prop. React's reconciliation process uses the key to determine when to create a new instance versus reusing the old one.
+
 export const routes = [
   {
     path: "/home",
@@ -34,12 +38,16 @@ export const routes = [
   {
     path: "/train",
     name: "Train",
-    component: <SelectionTask stories={train_stories_shuffle} mode="train" />,
+    component: (
+      <SelectionTask key="train" stories={train_stories_shuffle} mode="train" />
+    ),
   },
   {
     path: "/task",
     name: "Task",
-    component: <SelectionTask stories={stories_shuffle} mode="task" />,
+    component: (
+      <SelectionTask key="task" stories={stories_shuffle} mode="task" />
+    ),
   },
   {
     path: "/end",
