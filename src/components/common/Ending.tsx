5dd23c5b69616362b77fcb8ce49a08619df9158a
SonghaiFan <songhai.fan2022@gmail.com>
2023-11-22 07:49:47 +1100

    dd

--- src/components/common/Ending.tsx
+++ src/components/common/Ending.tsx
@@ -133,7 +133,7 @@ const Ending: React.FC = () => {
             />
             <button
               onClick={handleEmailSubmit}
-              className="px-4 py-2 font-semibold text-white bg-green-500rounded-md flex items-center justify-center"
+              className="px-4 py-2 font-semibold text-white bg-green-500 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 flex items-center justify-center"
             >
               <FiMail className="mr-2" /> Submit Email
             </button>
