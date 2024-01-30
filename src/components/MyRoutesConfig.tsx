a9ae2c3d1eb2c1dfe50f7e5d15c1da7fbe3bd42b
SonghaiFan <songhai.fan2022@gmail.com>
2024-01-30 14:45:49 +1100

    setc

--- src/components/MyRoutesConfig.tsx
+++ src/components/MyRoutesConfig.tsx
@@ -5,9 +5,9 @@ import Final from "./common/Final";
 import MarkdownRenderer from "./common/MarkdownRenderer";
 
 import train_stories from "./data/stories/combined_stories_train.json";
-import stories from "./data/stories/combined_stories_a.json";
+// import stories from "./data/stories/combined_stories_a.json";
 // import stories from "./data/stories/combined_stories_b.json";
-// import stories from "./data/stories/combined_stories_c.json";
+import stories from "./data/stories/combined_stories_c.json";
 
 // use loadsash to shuffle the stories
 export const train_stories_shuffle = _.shuffle(train_stories);
